2965339f4f14e5b0f285fb036e18c557fb876cf2
SheisThanhHien <hien.nguyenthanh@ncc.asia>
2026-04-26 16:53:51 +0700

    add: constants file

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
-BASE_URL = "https://mezon.ai/"
+# BASE_URL = "https://mezon.ai/"
 # URL Test Dev
-# BASE_URL = "https://dev-mezon.nccsoft.vn/"
+BASE_URL = "https://dev-mezon.nccsoft.vn/"
 
 # Account Test Prod
 # EMAIL = "yocosa3965@crsay.com"
