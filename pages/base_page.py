64a0ef2512c827650f2453ede17fe7a3dd471b50
sheisThanhHien <hien.nguyenthanh@ncc.asia>
2026-07-22 16:15:16 +0700

    Add action into file base

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,9 @@
 import time
 
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
 
 
 class BasePage:
@@ -31,3 +34,63 @@ class BasePage:
             time.sleep(self.POLL_INTERVAL)
 
         raise AssertionError(error_message)
+
+
+
+    # ================================================
+    # Mouse Actions
+    # ================================================
+    def hover(self, element):
+        """Hover chuột vào element"""
+        ActionChains(self.driver).move_to_element(element).perform()
+
+    def click_element(self, element):
+        """Click trực tiếp vào WebElement"""
+        element.click()
+
+    def double_click(self, element):
+        """Double click"""
+        ActionChains(self.driver).double_click(element).perform()
+
+    def right_click(self, element):
+        """Right click"""
+        ActionChains(self.driver).context_click(element).perform()
+
+    def drag_and_drop(self, source, target):
+        """Kéo thả"""
+        ActionChains(self.driver).drag_and_drop(source, target).perform()
+
+    # ========================
+    # Keyboard Actions
+    # ========================
+
+    def press_enter(self, element):
+        element.send_keys(Keys.ENTER)
+
+    def press_shift_enter(self, element):
+        element.send_keys(Keys.SHIFT, Keys.ENTER)
+
+    def press_escape(self, element):
+        element.send_keys(Keys.ESCAPE)
+
+    def press_tab(self, element):
+        element.send_keys(Keys.TAB)
+
+    # ========================
+    # Scroll
+    # ========================
+
+    def scroll_to_element(self, element):
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView({block:'center'});",
+            element
+        )
+
+    def scroll_to_bottom(self):
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    def scroll_to_top(self):
+        self.driver.execute_script("window.scrollTo(0, 0);")
+
+    def scroll_to_middle(self):
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 2);")
